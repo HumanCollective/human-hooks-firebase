e3dc6e76777580eb4cbdf4763cd5b44fe6c57883
Mitchell Butler <mitch@humancollective.co>
2021-03-16 16:36:33 -0400

    [stash] backup

--- packages/native/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import auth from '@react-native-firebase/auth'
-
-export { auth }
-
-export * from './shared/useAuth'
-export * from './shared/useAuthedCollection'
-export * from './shared/useAuthedCollectionCount'
-export * from './shared/useAuthedDocument'
